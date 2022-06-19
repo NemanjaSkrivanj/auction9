911fec8b81d4fbf1b7e3ec584b1b840df3185ba1
NemanjaSkrivanj <nemanjas2000@gmail.com>
2022-06-19 11:45:38 +0200

    added route for auction form

--- auction9-frontend/src/dashboard/LandingPage.jsx
+++ auction9-frontend/src/dashboard/LandingPage.jsx
@@ -8,6 +8,8 @@ import Link from '@mui/material/Link';
 import Navigator from './Navigator';
 import Content from './Content';
 import Header from './Header';
+import AuctionForm from '../components/CreateAuctionForm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function Copyright() {
   return (
@@ -199,7 +201,12 @@ export default function Paperbase() {
         <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
           <Header onDrawerToggle={handleDrawerToggle} />
           <Box component="main" sx={{ flex: 1, py: 6, px: 4, bgcolor: '#eaeff1' }}>
-            <Content />
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Content/>}></Route>
+                <Route path="/createAuction" element={<AuctionForm/>}></Route>
+              </Routes>
+            </BrowserRouter>
           </Box>
           <Box component="footer" sx={{ p: 2, bgcolor: '#eaeff1' }}>
             <Copyright />
